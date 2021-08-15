58537f58a1c5e5bd73c8b58dee92e483e4d5e322
hengyuni <hengyuni@gmail.com>
2021-08-15 18:22:14 -0400

    sliding window patter

--- script.js
+++ script.js
@@ -194,7 +194,7 @@ const isAlphaNumeric = (char) => {
 // SECTION 2.6 Problem solving patters //
 /////////////////////////////////////////
 
-// Frequency Counter //
+//  PATTERN 1: FREQUENCY COUNTER //
 /* Uses objects ot sets to collect values/frequency of values */
 /* Can often avoid the need for a nested loops or O(N^2) operations with arrays/strings */
 
@@ -272,12 +272,103 @@ function validAnagram(first, second) {
 
 //////////////////////////
 
-// MULTIPLE POINTERS //
+// PATTERN 2: MULTIPLE POINTERS //
 // creating ponters or values that correspond to an index or position and move towards the beginning end or middle based on a certain condition
 // ** very efficient for solving problems with minimal space complexity as well
 
 // sum zero naive **O(n^2)
+function sumZero(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[i] + arr[j] === 0) {
+				return [arr[i], arr[j]];
+			}
+		}
+	}
+}
 // sum zero refactor **O(n)
+// time complexity O(n)
+// space complexity O(1)
+function sumZero2(arr) {
+	let left = 0;
+	let right = arr.length - 1;
+	while (left < right) {
+		let sum = arr[left] + arr[right];
+		if (sum === 0) {
+			return [arr[left], arr[right]];
+		} else if (sum > 0) {
+			right--;
+		} else {
+			left++;
+		}
+	}
+}
 
-const output = validAnagram("anagramsss", "nagaramsss");
+// Count Unique Values
+// implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted
+/*
+countUniqueValues([1,1,1,1,1,2]) // 2
+countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
+countUniqueValues([]) // 0
+countUniqueValues([-2,-1,-1,0,1]) // 4
+*/
+function countUniqueValues(arr) {
+	let i = 0;
+	if (arr.length === 0) {
+		return 0;
+	}
+	// j will start at index of 1 and iterate through the array
+	for (let j = 1; j < arr.length; j++) {
+		// if arr[i] is not equal to arr[j] move i up by 1, when equal make i = j
+		if (arr[i] !== arr[j]) {
+			i++;
+			arr[i] = arr[j];
+		}
+		// console.log(i, j);
+	}
+	return i + 1;
+}
+
+// PATTERN 3: SLIDING WINDOW //
+// this pattern involes creating a *window* which can either be an array or number from one position to another
+// depending on a certain condition, the window either increases or closes(and a new window is created)
+// very useful for keeping track of a subset of data in an array/string etc,
+
+// write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array
+
+// naiva solution **nested loop makes it O(n^2)
+function maxSubarraySum(arr, num) {
+	if (num > arr.length) {
+		return null;
+	}
+	var max = -Infinity;
+	for (let i = 0; i < arr.length - num + 1; i++) {
+		temp = 0;
+		for (let j = 0; j < num; j++) {
+			temp += arr[i + j];
+		}
+		if (temp > max) {
+			max = temp;
+		}
+	}
+	return max;
+}
+
+// refactored solution
+function maxSubarraySum2(arr, num) {
+	let maxSum = 0;
+	let tempSum = 0;
+	if (arr.length < num) return null;
+	for (let i = 0; i < num; i++) {
+		maxSum += arr[i];
+	}
+	tempSum = maxSum;
+	for (let i = num; i < arr.length; i++) {
+		tempSum = tempSum - arr[i - num] + arr[i];
+		maxSum = Math.max(maxSum, tempSum);
+	}
+	return maxSum;
+}
+
+const output = maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
 console.log(output);
